82364e712b4cfc10b61bf917957123877d5ba9c0
Hadi Sinaee <sinaee.hadi@gmail.com>
2024-10-18 18:15:04 -0700

    lazy: Disable change notification

--- lua/hadi/lazy.lua
+++ lua/hadi/lazy.lua
@@ -31,5 +31,7 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
-  change_detection = { enabled = false },
+  change_detection = {
+    notify = false,
+  },
 })
